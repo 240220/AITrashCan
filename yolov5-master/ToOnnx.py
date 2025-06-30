64f06df19ebedfc0e1ba6f735d6a25e3bb4fd115
Xiao Botong <169167843+240220@users.noreply.github.com>
2025-07-01 00:35:11 +0800

    Delete yolov5-master directory

--- yolov5-master/ToOnnx.py
+++
@@ -1,9 +0,0 @@
-import torch
-from ultralytics import YOLO
-
-device = torch.device('cuda:0')
-print('device', device)
-
-model = YOLO('best3.pt')
-path_out = model.export(format="onnx",opset=11)
-print("path_out:",path_out)
